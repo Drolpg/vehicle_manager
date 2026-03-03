a283dd21008f066a697306a5214d18e5fdc0a640
Leandro Nascimento <leandro.nascimento@rarolabs.com.br>
2026-03-03 09:24:47 -0300

    feat: Crialçao de CRUD

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,62 @@
+class CarsController < ApplicationController
+  before_action :set_car, only: %i[ show edit update destroy ]
+
+  def index
+    @cars = Car.all
+  end
+
+  def show
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def edit
+  end
+
+  def create
+    @car = Car.new(car_params)
+
+    respond_to do |format|
+      if @car.save
+        format.html { redirect_to @car, notice: "O carro foi criado com sucesso." }
+        format.json { render :show, status: :created, location: @car }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @car.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @car.update(car_params)
+        format.html { redirect_to @car, notice: "O carro foi atualizado com sucesso.", status: :see_other }
+        format.json { render :show, status: :ok, location: @car }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @car.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @car.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to cars_path, notice: "Carro foi deletado com sucesso.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :year, :price, :color)
+  end
+end
